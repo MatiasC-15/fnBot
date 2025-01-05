3f3b0058c53f4c189377c37b069d8109776755f5
MatiasC-15 <kenddrysilva15@gmail.com>
2025-01-05 14:08:02 -0500

    Update downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -1,102 +1,62 @@
-import fetch from 'node-fetch';
-import axios from 'axios';
+/* 
 
-let handler = async (m, { conn, command, args, text, usedPrefix }) => {
-if (!text) return conn.reply(m.chat, INGRESE EL NOMBRE DE ALGUNA CANCION, m, rcanal)
+❀ By JTxs
 
-await m.react('🕒');
-try {
-let api = await fetch(https://apis-starlights-team.koyeb.app/starlight/soundcloud-search?text=${encodeURIComponent(text)});
-let json = await api.json();
-let { url } = json[0];
+[ Canal Principal ] :
+https://whatsapp.com/channel/0029VaeQcFXEFeXtNMHk0D0n
 
-let api2 = await fetch(https://apis-starlights-team.koyeb.app/starlight/soundcloud?url=${url});
-let json2 = await api2.json();
+[ Canal Rikka Takanashi Bot ] :
+https://whatsapp.com/channel/0029VaksDf4I1rcsIO6Rip2X
 
-let { link: dl_url, quality, image } = json2;
+[ Canal StarlightsTeam] :
+https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S
 
-let audio = await getBuffer(dl_url);
+[ HasumiBot FreeCodes ] :
+https://whatsapp.com/channel/0029Vanjyqb2f3ERifCpGT0W
+*/
 
-let txt = *\- M U S I C -\*\n\n;
-    txt += ✩  *Título* : ${json[0].title}\n;
-    txt += ✩  *Calidad* : ${quality}\n;
-    txt += > 🚩 *${textbot}*
+// $ npm i btch-downloader
+// Package : "btch-downloader": "^2.3.2",
+// *[ ❀ PLAY ]*
 
-await conn.sendFile(m.chat, image, 'thumbnail.jpg', txt, m, null, rcanal);
-await conn.sendMessage(m.chat, { audio: audio, fileName: ${json[0].title}.mp3, mimetype: 'audio/mpeg' }, { quoted: m })
-
-await m.react('✅');
-} catch {
-await m.react('✖️');
-}}
-
-handler.help = ['soundcloud <búsqueda>']
-handler.tags = ['downloader']
-handler.command = ['lol']
-
-export default handler
-
-const getBuffer = async (url, options) => {
-try {
-const res = await axios({
-method: 'get',
-url,
-headers: {
-'DNT': 1,
-'Upgrade-Insecure-Request': 1,
-},
-...options,
-responseType: 'arraybuffer',
-});
-return res.data;
-} catch (e) {
-console.log(Error : ${e});
-}
-};
-
-/*
-//Instalar la dependencia Node-id3 🙃
-//Use math por problemas de que algunos audios no se envian
-//La segunda url si descarga los datos de la cancion para eso tienes que ingresar a Souncloud la musica que quieres descargar ingresas y copias el link y lo pegas en la segunda url :) 
-//el buscador aun no tiene permisos para ir directamente a la cancion y obtener el link directamente a la cancion por eso es que algunos audios no son enviados
+import { youtube } from 'btch-downloader'
+import yts from 'yt-search'
 import axios from 'axios'
-import fs from 'fs'
-import nodeID3 from 'node-id3'
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(m.chat, 🚩 Ingrese el nombre de la cancion de *Soundcloud.*, m, rcanal)
-await m.react('🕓')
+if (!text) return conn.reply(m.chat, `❀ Ingresa el texto de lo que quieres buscar`, m)
+
 try {
-let { data: results } = await axios.get(https://apis-starlights-team.koyeb.app/starlight/soundcloud-search?text=${text}, { headers: { 'Content-Type': 'application/json' } })
-let randoms = results[Math.floor(Math.random() * results.length)]
-let { data: sm } = await axios.get(https://apis-starlights-team.koyeb.app/starlight/soundcloud?url=${randoms.url}, { headers: { 'Content-Type': 'application/json' }})
-let mpeg = await axios.get(sm.audio, { responseType: 'arraybuffer' })
-let img = await axios.get(randoms.image, { responseType: 'arraybuffer' })
-let mp3 = ${sm.title}.mp3
-fs.writeFileSync(mp3, Buffer.from(mpeg.data))
-let tags = {
-title: sm.title,
-artist: sm.creator, 
-image: Buffer.from(img.data) 
-}
-nodeID3.write(tags, mp3)
-let txt = *\- S O U N C L O U D - M U S I C -\*\n\n
-txt += 🍘• *Nombre:* ${randoms.title}\n
-txt += 🍘• *Artista:* ${randoms.artist}\n
-txt += 🍘• *Duracion:* ${randoms.duration}\n
-txt += 🍘• *Reproducciones:* ${randoms.repro}\n
-txt += 🍘• *Link:* ${randoms.url}\n\n
-txt += 🚩 Powered By Starlights Team
-await conn.sendFile(m.chat, randoms.image, 'thumb.jpg', txt, m)
-await conn.sendMessage(m.chat, { audio: fs.readFileSync(mp3), fileName: ${sm.title}.mp3, mimetype: 'audio/mpeg' }, { quoted: m })
-fs.unlinkSync(mp3)
-await m.react('✅')
-} catch {
-await m.react('✖️')
+let ytsres = await yts(text)
+let video = ytsres.videos[0]
+
+if (!video) return conn.reply(m.chat, `❀ Sin resultados obtenidos :(`, m)
+
+let { title, duration, views, ago, author, thumbnail, url } = video
+let HS = `- *Titulo :* ${title}
+- *Duracion :* ${duration.timestamp}
+- *Visitas :* ${views.toLocaleString()}
+- *Subido :* ${ago}
+- *Autor :* ${author.name}`
+
+ 
+await conn.sendMessage(m.chat, {text: HS,
+contextInfo: { externalAdReply: {
+title: `${title}`, body: `${author.name}`,
+thumbnailUrl: thumbnail, sourceUrl: url,
+mediaType: 1, renderLargerThumbnail: true
+}}}, { quoted: m })
+
+let data = await youtube(url)
+
+if (!data || !data.mp3) return conn.reply(m.chat, `❀ Descarga fallida :(`, m)
+
+await conn.sendMessage(m.chat, { audio: { url: data.mp3 }, mimetype: 'audio/mpeg', }, { quoted: m })
+//data.mp4 para video :v
+} catch (error) {
+console.error(error)
 }}
-handler.help = ['soundcloud <búsqueda>']
-handler.tags = ['downloader']
-handler.command = ['soundcloud', 'sound', 'play']
-handler.register = true
-//handler.limit = 3
-export default handler*/
+
+handler.command = ['play']
+
+export default handler
