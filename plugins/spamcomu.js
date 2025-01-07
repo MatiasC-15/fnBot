31c5a494bf05c1c44e55bd64d9e9f7720f9ed8db
MatiasC-15 <kenddrysilva15@gmail.com>
2025-01-07 16:42:49 -0500

    Create spamcomu.js

---
+++ plugins/spamcomu.js
@@ -0,0 +1,27 @@
+const handler = async (m, {conn}) => {
+  m.reply(global.SpamLink);
+};
+handler.command = /^(SpamComu)$/i;
+export default handler;
+global.SpamLink = `
+
+𝗟𝗜𝗡𝗞𝗦 𝗗𝗘 𝗖𝗢𝗠𝗨𝗡𝗜𝗗𝗔𝗗𝗘𝗦
+
+𝑪𝑶𝑴𝑼𝑵𝑰𝑫𝑨𝑫 𝑭𝑵 ⚜️
+https://chat.whatsapp.com/Df90RzuAvF20VKGkxlSPJG
+
+⚜️ 𝑺𝑪𝑹𝑰𝑴𝑺 𝑭𝑵 ⚜️
+https://chat.whatsapp.com/EK3f2VCVG4GA88VQ2NkYCp
+
+⚜️ 𝑺𝑪𝑹𝑰𝑴𝑺 𝑬𝑵 𝑫𝑼𝑶 𝑭𝑵 ⚜️
+https://chat.whatsapp.com/HVKMaBSI9WGHFp5Rfnxfqu
+
+⚜️ 𝑪𝑼𝑨𝑫𝑹𝑰 𝒚 𝑯𝑬𝑿𝑨𝑮𝑶 𝑭𝑵 ⚜️
+https://chat.whatsapp.com/BthEQP8AF8C0ni7f29VtP4
+
+⚜️ 𝑳𝑰𝑮𝑨𝑺 𝑪𝑳𝑲 𝑭𝑵  ⚜️
+https://chat.whatsapp.com/KvlZGoG27GU11tOFE9JBvw
+
+⚜️ 𝑸𝑼𝑬𝑴𝑨𝑵𝑫𝑶 𝑵𝑶𝑹𝑻𝑬 𝑭𝑵 ⚜️
+https://chat.whatsapp.com/Gy5SNwLTR6S1YZ1BtV3TwC
+`;
